1981c94aa1ad3a43456c8ba2b034938e1403ac3b
ShayanAkhun <littelmaster391@gmail.com>
2023-02-14 18:44:41 +0500

    deployed to firebase

--- src/Components/Rows/Row.jsx
+++ src/Components/Rows/Row.jsx
@@ -18,17 +18,18 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map((movie) => {
-          console.log(movie);
-          return (
-            <img
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              key={movie.id}
-              src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
-              alt={movie.name}
-            />
-          );
-        })}
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                key={movie.id}
+                src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                alt={movie.name}
+              />
+            )
+        )}
       </div>
     </div>
   );
