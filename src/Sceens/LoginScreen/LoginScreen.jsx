a1ee4bbd00671021f2e18f30992f797421cac0f9
ShayanAkhun <littelmaster391@gmail.com>
2023-02-19 04:39:11 +0500

    loginscreen added

--- src/Sceens/LoginScreen/LoginScreen.jsx
+++ src/Sceens/LoginScreen/LoginScreen.jsx
@@ -13,6 +13,23 @@ function LoginScreen() {
         <button className="loginScreen__button">Sign In</button>
         <div className="loginScreen__graident"></div>
       </div>
+      <div className="loginScreen__body">
+        <>
+          <h1>Unlimited flims, TV programmes and more</h1>
+          <h2>Watch anywhere. cancel anytime</h2>
+          <h3>
+            Ready to watch? Enter your email to create or restart your
+            membership
+          </h3>
+
+          <div className="loginScreen__input">
+            <form>
+              <input type="email" placeholder="Email Address" />
+              <button className="loginScreen__getStarted">GET STARTED</button>
+            </form>
+          </div>
+        </>
+      </div>
     </div>
   );
 }
